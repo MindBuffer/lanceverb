e08a01297885b931714ee9f5356cbd059fc7ddb6
mitchmindtree <mitchell.nordine@gmail.com>
2015-06-08 12:59:11 +1000

    Fixed feedback issue. IT WWOOORRKKSSS

--- examples/test.rs
+++ examples/test.rs
@@ -1,37 +1,38 @@
+//! 
+//! Simple example that applies reverb to the default input device's stream and passes it straight
+//! to the default output device's stream.
+//!
+
 
 extern crate dsp;
 extern crate lanceverb;
 
-use dsp::{CallbackFlags, CallbackResult, Graph, Node, Sample,
-          Settings, SoundStream, StreamParams, Wave};
+use dsp::{CallbackFlags, CallbackResult, Node, Sample, Settings, SoundStream, StreamParams};
 use lanceverb::Reverb;
 
 
 fn main() {
 
+    // Construct the default reverb.
     let mut verb = Reverb::new();
-    // verb.bandwidth(0.9995);
-    // verb.decay(0.9);
-    // verb.damping(0.3);
 
-    let callback = Box::new(move |input: &[f32], in_settings: Settings,
+    // Callback used to construct the duplex sound stream.
+    let callback = Box::new(move |input: &[f32], _in_settings: Settings,
                                   output: &mut[f32], out_settings: Settings,
-                                  dt: f64,
+                                  _dt: f64,
                                   _: CallbackFlags| {
-
         Sample::zero_buffer(output);
-
-        for (out_sample, in_sample) in output.iter_mut().zip(input.iter()) {
-            //println!("{:?}", in_sample);
-            *out_sample = if in_sample.abs() > 1.0 { 1.0 } else { *in_sample };
+        for (out_s, in_s) in output.iter_mut().zip(input.iter()) {
+            *out_s = if *in_s > 1.0 { 1.0 }
+                     else if *in_s < -1.0 { -1.0 }
+                     else { *in_s };
         }
 
         verb.audio_requested(output, out_settings);
-
         CallbackResult::Continue
     });
 
-    // Construct the stream and handle any errors that may have occurred.
+    // Construct the stream with default parameters.
     let stream = SoundStream::new()
         .duplex(StreamParams::new(), StreamParams::new())
         .run_callback(callback)
